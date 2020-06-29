04508fc4b7a6c14e2822c3c6ff0804718d71f78f
Godwin <godwin.terence@gmail.com>
2020-06-30 01:36:34 +0530

    Added my App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Happy Coding
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      datasource: {}
+
+    };
+  }
+
+  async componentDidMount(){
+    try {
+			const response = await fetch('https://x5uryqu5pl.execute-api.us-east-1.amazonaws.com/Production');
+			let responseJson = await response.json();
+			this.setState(
+				{
+					isLoading: false,
+					dataSource: responseJson
+				},
+				function() {}
+			);
+		} catch (error) {
+			console.error(error);
+		}
+  }
+
+  render(){
+    let { dataSource } = this.state;
+		if (this.state.isLoading) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div>
+					{dataSource.Items.map(item => (
+						<div key={item.CardId}>
+							<h1>{item.CardColor}</h1>
+							<li>{item.CardCost}</li>
+							<li>{item.CardText}</li>
+						</div>
+					))}
+				</div>
+			);
+		}
+	}
+
+  
+
 }
+
+
 
 export default App;
